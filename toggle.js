43db51568430864332949967e5859302d4d22813
Devin Berkani <devinberkani@gmail.com>
2022-04-28 08:35:40 -0400

    complete new portfolio website

--- toggle.js
+++ toggle.js
@@ -6,9 +6,19 @@ const getElement = (selector) => {
 
 const navToggle = getElement('.nav-toggle');
 const links = getElement('.links');
+const main = getElement('#home');
 
+// close navbar when navbar icon is clicked
 const toggleClasses = () => {
   links.classList.toggle('show-links');
 };
 
+// close navbar when anywhere else is clicked
+const closeNavbar = () => {
+  if (links.classList.contains('show-links')) {
+    toggleClasses();
+  }
+};
+
 navToggle.addEventListener('click', toggleClasses);
+main.addEventListener('click', closeNavbar);
